806e185a6926b84f42b509996a8aa2ac8c0c5488
JustNunuz <nunudzaimrewa222@gmail.com>
2025-01-05 09:40:55 +0200

    updated readme and log files

--- honeypy.py
+++ honeypy.py
@@ -1,37 +1,31 @@
 import argparse
-from ssh_honeypot import *
+from ssh_honeypot import honeypot as ssh_honeypot
+from web_honeypot import run_web_honeypot
 
-#Parse Arguements
+# Parse Arguments
 
-if __name__== "__main__":
-    parser= argparse.ArgumentParser()
-    
-    parser.add_argument("-a","--address",type=str, required=True)
-    parser.add_argument("-p", "--port", type=int,required=True)
-    parser.add_argument("-u","--username", type=str)
-    parser.add_argument("-pw","--password", type=str)
-    
-    parser.add_argument('-s',"-ssh",action="store_true")
-    parser.add_argument("-w","http",action="store_true")
-    
-    
-    args= parser.parse_args()
-    
-    
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("-a", "--address", type=str, required=True)
+    parser.add_argument("-p", "--port", type=int, required=True)
+    parser.add_argument("-u", "--username", type=str)
+    parser.add_argument("-pw", "--password", type=str)
+
+    parser.add_argument('-s', "--ssh", action="store_true")
+    parser.add_argument("-w", "--http", action="store_true")
+
+    args = parser.parse_args()
+
     try:
         if args.ssh:
             print("[ - ] Running SSH honeypot ...")
-            honeypot(args.address,args.port,args.username,args.password) 
-            if not args.username:
-                username = None
-            if not args.password:
-                password= None  
+            ssh_honeypot(args.address, args.port, args.username, args.password)
         elif args.http:
-            print("[ - ] Runnign HTTp Wordpress Hoenypot...")
-            
-            pass
+            print("[ - ] Running HTTP Wordpress Honeypot ...")
+            run_web_honeypot(port=args.port, input_username=args.username, input_password=args.password)
         else:
             print("Choose a honeypot to run")
-    except:
-        print("\n Exiting honeypy")
+    except Exception as e:
+        print(f"\n Exiting honeypy due to error: {e}")
         pass
